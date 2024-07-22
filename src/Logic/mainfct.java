a66e8b04d414a9d6b92fe76b3d0bc93b82d1cd21
Juan Guzman <juanjguzman15@gmail.com>
2024-07-21 22:25:35 -0500

    commit_Eclipse

---
+++ src/Logic/mainfct.java
@@ -0,0 +1,17 @@
+package Logic;
+
+import javax.swing.JFrame;
+import GUI.ScreenFCT;
+
+public class mainfct {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("ScreenFCT");
+        
+        frame.setContentPane(new ScreenFCT());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 500); 
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
