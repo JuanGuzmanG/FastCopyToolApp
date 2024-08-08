a5aefe14f50362b1eb5fc3dc14e156107f99b028
Juan Guzman <juanjguzman15@gmail.com>
2024-08-08 14:56:41 -0500

    update

---
+++ src/GUI/Main.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Main {
+    private JButton button2;
+    private JButton button3;
+    private JButton ADDLISTButton;
+    private JButton COPYButton2;
+    private JButton CLEARButton2;
+    private JButton COPYButton1;
+    private JButton CLEARButton1;
+    private JButton COPYButton;
+    private JButton CLEARButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JList list1;
+}
--- src/Logic/mainfct.java
+++ src/Logic/mainfct.java
@@ -1,18 +1,11 @@
 package Logic;
 
 import javax.swing.JFrame;
-import GUI.ScreenFCT;
 
 
 public class mainfct {
     public static void main(String[] args){
-        JFrame frame = new JFrame("ScreenFCT");
-        
-        frame.setContentPane(new ScreenFCT());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 550);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+
     }
 }
 
